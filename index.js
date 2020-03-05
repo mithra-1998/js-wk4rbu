e2af662e87d1336a50c214eb4bdbe95caab25fd0
mithra-1998 <mitworkmail@gmail.com>
2020-03-05 15:02:30 +0530

    1 file modified

--- index.js
+++ index.js
@@ -8,3 +8,11 @@ let stale = ["Emil", "Tobias", "Linus"];
 console.log(hege.concat(stale)); //Output : ["Cecilie", "Lone", "Emil", "Tobias", "Linus"]
 
 // List all array items, with keys and values:
+let fruits = ["apple", "orange", "cherry"];
+fruits.forEach(myFunction);
+
+function myFunction(item, index) {
+  console.log(item +"->"+index) //apple->0 orange->1 cherry->2
+}
+
+//to get the square root of each element in the array using Map function 
