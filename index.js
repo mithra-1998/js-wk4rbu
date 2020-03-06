e93b3de7f276292249b3e53a7c066af0540ff3fd
mithra-1998 <mitworkmail@gmail.com>
2020-03-06 09:33:12 +0530

    1 file modified

--- index.js
+++ index.js
@@ -2,12 +2,12 @@
 // Array methods 
 
 
-//Concat two arrays
+//Concat two arrays ->concat
 let hege = ["Cecilie", "Lone"];
 let stale = ["Emil", "Tobias", "Linus"];
 console.log(hege.concat(stale)); //Output : ["Cecilie", "Lone", "Emil", "Tobias", "Linus"]
 
-// List all array items, with keys and values:
+// List all array items, with keys and values: ->forEach
 let fruits = ["apple", "orange", "cherry"];
 fruits.forEach(myFunction);
 
@@ -15,42 +15,42 @@ function myFunction(item, index) {
   console.log(item +"->"+index) //Output : apple->0 orange->1 cherry->2
 }
 
-//to get the square root of each element in the array using Map function 
+//to get the square root of each element in the array using Map function ->map
 let numbers = [4, 9, 16, 25];
 console.log(numbers.map(Math.sqrt)); //Output : [2, 3, 4, 5]
 
 
-//array filter using a condition 
+//array filter using a condition ->filter
 let ages = [32, 33, 12, 40];
 function checkAdult(age) {
   return age >= 18;
 }
 console.log(ages.filter(checkAdult)); //Output : [32, 33, 40]
 
-// Check if an array includes "Mango"
+// Check if an array includes "Mango" ->includes
 let fruits = ["Banana", "Orange", "Apple", "Mango"];
 console.log(fruits.includes("Mango")); //Output : true
 
-// Search an array for the item "Apple" 
+// Search an array for the item "Apple" ->indexOf
 let fruits = ["Banana", "Orange", "Apple", "Mango"];
 console.log(fruits.indexOf("Apple")); //Output : 2
 
 
-// Get the index of the first element in the array that has a value of 18 or more
+// Get the index of the first element in the array that has a value of 18 or more->findIndex
 let ages = [3, 10, 18, 20];
 function checkAdult(age) {
   return age >= 10;
 }
 console.log(ages.findIndex(checkAdult)); //Output : 1
 
-// Get the value of the first element in the array that has a value of 18 or more
+// Get the value of the first element in the array that has a value of 18 or more->find
 let ages = [3, 10, 18, 20];
 function checkAdult(age) {
   return age > 15;
 }
 console.log(ages.find(checkAdult)); //Output : 18
 
-//Subtract the numbers in the array, starting from the beginning
+//Subtract the numbers in the array, starting from the beginning->reduce
 let numbers = [175, 50, 25];
 console.log(numbers.reduce(myFunc));
 
@@ -59,19 +59,33 @@ function myFunc(total, num) {
 
 }
 
-//Select elements from an array
+//Select elements from an array->slice
 let fruits = ["Banana", "Orange", "Lemon", "Apple", "Mango"];
 console.log(fruits.slice(1, 4)); // ["Orange", "Lemon", "Apple"]
 
 
+//Add items to the array -> splice
+let fruits = ["Banana", "Orange", "Apple", "Mango"];
+console.log(fruits.splice(2, 0, "Lemon", "Kiwi"))
 
 
-splice
-sort
-reverse
-concat
-pop
-push
-shift
-unhift
+
+
+// splice
+// sort
+// reverse
+// concat
+// pop
+// push
+// shift
+// unhift
+
+//========================================================================================
+
+
+// const array = [1,2,3,4,5];
+// const obj = [{name:'1'}, {name:'2'}, {name:'3'}, {name:'4'}, {name: '5'}]
+// for(let a of obj) {
+//   console.log(a)
+// }
 
